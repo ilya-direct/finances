a03c944473b27835c91216cad0158f405a4c7c85
Mifotake <ilya-direct@ya.ru>
2015-04-07 00:10:59 +0300

    del

--- controllers/CompanyController.php
+++ controllers/CompanyController.php
@@ -3,11 +3,21 @@
 namespace app\controllers;
 use yii\web\Controller;
 use app\models\company;
-
+use yii\web\YiiAsset;
+use Yii;
 
 class CompanyController extends Controller{
+
     public function actionIndex(){
-        $companies= Company::find()->all();
-        return $this->render('index',['companies'=>$companies]);
+
+        //$companies= Company::find()->all();
+	    $connection=Yii::$app->db;
+	    $sql='select name,id,backtrace from company where id<5';
+	    $sql='insert into company (name,link) values("reStore","reStore.com")';
+	    $command=$connection->createCommand($sql);
+	    //$dataReader=$command->queryAll();
+	    $dataReader=$command->execute();
+	    //$dataReader=$command->queryColumn();
+        return $this->render('index',['companies'=>$dataReader]);
     }
 }
