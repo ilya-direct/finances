c7b5cb718bd756c0639933afd371801629ed550e
ilya <ilya-direct@ya.ru>
2015-04-07 21:53:43 +0400

    fixed

--- controllers/CompanyController.php
+++ controllers/CompanyController.php
@@ -13,11 +13,21 @@ class CompanyController extends Controller{
         //$companies= Company::find()->all();
 	    $connection=Yii::$app->db;
 	    $sql='select name,id,backtrace from company where id<5';
-	    $sql='insert into company (name,link) values("reStore","reStore.com")';
-	    $command=$connection->createCommand($sql);
+	   // $sql='insert into company (name,link) values("reStore","reStore.com")';
+
+	    $command=$connection->createCommand();
+	    $companies=(new \yii\db\Query())->select('name,link,backtrace')->from('company')->where('id>10')->all();
+	    $companies=(new \yii\db\Query())
+		        ->select('name,link,backtrace')
+		        ->from('company c')
+		        ->join('company_assign ca', 'ca.company_id=c.id')
+		        ->join('service s', 'ca.service_id=s.id')
+		        ->join('device d', 'ca.device_id=d.id')
+		        ->where('c.id=1')->all();
+
 	    //$dataReader=$command->queryAll();
-	    $dataReader=$command->execute();
+	    //$dataReader=$command->execute();
 	    //$dataReader=$command->queryColumn();
-        return $this->render('index',['companies'=>$dataReader]);
+        return $this->render('index',['companies'=>$companies]);
     }
 }
