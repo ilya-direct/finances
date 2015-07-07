bc8ac97f0ff91174b439a68efb317daf790eaaa7
Mifotake <ilya-direct@ya.ru>
2015-07-07 15:11:15 +0300

    phpexcel

--- commands/WalletController.php
+++ commands/WalletController.php
@@ -6,7 +6,10 @@ class WalletController extends \yii\console\Controller
 {
     public function actionIndex()
     {
-        echo "Hello";
+	    $DIR=dirname(__FILE__);
+	    $objPHPExcel = \PHPExcel_IOFactory::load($DIR. '/2014.10.xlsm');
+	    $sheetData = $objPHPExcel->getActiveSheet()->toArray(null, false, true, true);
+	    print_r($sheetData);
     }
 
 }
