68610474afc3839e9a123a1798df54907135708a
Mifotake <ilya-direct@ya.ru>
2015-07-09 20:38:14 +0300

    xlsm2csv

--- commands/WalletController.php
+++ commands/WalletController.php
@@ -19,15 +19,16 @@ class WalletController extends \yii\console\Controller
     }
     public function actionDbxdownload()
     {
+	    //TODO: добавить константу token в конфиг
 	    $token='OprJKfb4QroAAAAAAAAFZw2tIxGlGCVvvqWn-58KmhEhazh_vSdUvUtpJ_JBTZDS';
 	    $client=new  dbx\Client($token,'directapp','UTF-8');
 
-	    $download_path=Yii::getAlias('@data').DIRECTORY_SEPARATOR.'finance_download';
+	    $download_path=Yii::getAlias('@finance_download_path');
 
-	    /*
-	    if(!is_dir($download_path) and !mkdir($download_path,0777,true))
-		    throw new Exception('can\'t create download directory');
-	    */
+		//TODO: Заменить is_dir функцией из библиотеки YII
+	    if(!is_dir($download_path) and !\yii\helpers\FileHelper::createDirectory($download_path,0777,true))
+		    throw new \Exception('can\'t create download directory');
+
 
 	    $finances=$client->getMetadataWithChildren('/finances')['contents'];
 	    $finances=array_map(function($el){
@@ -37,12 +38,15 @@ class WalletController extends \yii\console\Controller
 
 	    $recs=DB\DbxFinance::find()->all();
 	    foreach( $recs as $rec){
+
+		    //TODO: убрать строку ниже, добавить её в AR форматный вывод поля
 		    $rec->month=str_replace(' ','0',\sprintf('%2.d', $rec->month));
 		    $yearmonth=$rec->year.'.'.$rec->month;
-		    $info=search_info($yearmonth,$finances);
+		    //TODO: решить проблему с API и убрать функцию SEARCH_INFO
+		    $info=$this->search_info($yearmonth,$finances);
 		    if(!$info) throw new \Exception('not found file '. $yearmonth);
 
-		    $download_filename=$download_path.DIRECTORY_SEPARATOR.$yearmonth.'.raw';
+		    $download_filename=$download_path.DIRECTORY_SEPARATOR.$yearmonth.'.xlsm';
 		    if($info['modified']>$rec->download_time || $rec->exists==0){
 			    $rec->download_time=date('Y-m-d H:i:s');
 			    $rec->modified_time=$info['modified'];
@@ -57,14 +61,47 @@ class WalletController extends \yii\console\Controller
 		    }
 	    }
     }
-}
+	private function search_info($pattern,$finances){
+		foreach($finances as $fin){
+			if(preg_match('/'.$pattern.'/',$fin['path'])){
+				return $fin;
+			}
+		}
+		return false;
+	}
 
 
-function search_info($pattern,$finances){
-	foreach($finances as $fin){
-		if(preg_match('/'.$pattern.'/',$fin['path'])){
-			return $fin;
+	public function actionXlsm2csv(){
+
+		$input_path=Yii::getAlias('@finance_download_path');
+		if(!is_dir($input_path))
+			throw new \Exception('can\'t find input directory');
+
+		$output_path=Yii::getAlias('@finance_csv');
+		if(!is_dir($output_path) and !\yii\helpers\FileHelper::createDirectory($output_path,0777,true))
+			throw new \Exception('can\'t create output directory');
+
+
+		$recs=DB\DbxFinance::findAll(['exists'=>1,'csv_converted'=>0]);
+		foreach($recs as $rec){
+			//TODO: убрать строку ниже, добавить её в AR форматный вывод поля
+			$rec->month=str_replace(' ','0',\sprintf('%2.d', $rec->month));
+			$file_name=$rec->year.'.'.$rec->month;
+			$input_filepath=$input_path.'/'.$file_name.'.xlsm';
+			if(file_exists($input_filepath)){
+				$objPHPExcel = \PHPExcel_IOFactory::load($input_filepath);
+				$objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'CSV');
+				$objWriter->setDelimiter(";");
+				$objWriter->setEnclosure("");
+				$objWriter->setPreCalculateFormulas(false);
+				$output_filepath=$output_path.'/'.$file_name.'.csv';
+				$objWriter->save($output_filepath);
+				$rec->csv_converted=1;
+				$rec->update();
+				echo "file $file_name converted\n";
+			}else
+				//$DB->set_field('dbx_finance','exists',0,array('id'=>$rec->id));
+				throw new \Exception("file $input_filepath not found\n");
 		}
 	}
-	return false;
 }
